2cb210c9aa78528b25d738a3e0b85a2c0368daa0
Ruirui Liu <liurui0891@gmail.com>
2017-05-03 09:13:42 -0400

    xml solution

---
+++ ltcStopP/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Xsl;   // Xslt types
+using System.IO;        // Directory class
+
+namespace ltcStopP
+{
+    class Program
+    {
+        private const string XML_FILE = @"..\..\..\ltcstops.xml";
+        private const string HTML_FILE = @"..\..\..\ltcstop.html";
+        private const string XSLT_FILE = @"..\..\..\ltcstop.xslt";
+        static void Main(string[] args)
+        {
+            // Display a title
+            Console.WriteLine("London Transit Commission Bus Stop Report");
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("Provide a street name or partial street name exactly as it appears in the xml file");
+         
+            // Enter the information
+            Console.Write("\nEnter a street name: ");
+            string streetName = Console.ReadLine().ToUpper().Trim(); 
+
+           
+            try
+            {
+                // Create a new XslTransform object and load the style sheet
+                XslCompiledTransform xslt = new XslCompiledTransform();
+                xslt.Load(XSLT_FILE);
+
+                // Set-up an XsltArgumentList object to pass to the 
+                // XSLT file's 'character' parameter
+                XsltArgumentList xsltArgList = new XsltArgumentList();
+                xsltArgList.AddParam("street", "", streetName);
+
+                // Execute the transformation 
+                FileStream outFile = File.Create(HTML_FILE);
+                xslt.Transform(XML_FILE, xsltArgList, outFile);
+                outFile.Close();
+                // Display the transformed XML file in Internet Explorer
+
+                // The rutime folder used by the Internet Explorer (IE) program is 
+                // different from the one used by our C# program. So we're going to give
+                // IE the absolute path to the XML file by using the GetCurrentDirectory() method.
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo.FileName = "iexplore";
+                proc.StartInfo.Arguments = Directory.GetCurrentDirectory().ToString() + "\\" + HTML_FILE;
+                proc.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: {0}", ex.Message);
+            }
+
+        }//end main
+    }//end class
+}//end namespace
---
+++ ltcstop.xslt
@@ -0,0 +1,82 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+    xmlns:msxsl="urn:schemas-microsoft-com:xslt" exclude-result-prefixes="msxsl"
+>
+    <xsl:output method="html" indent="yes"/>
+
+  <xsl:param name="street"/>
+
+  <xsl:template match="/">
+    <html>
+      <body>
+        <h1>
+         <xsl:if test="$street != ''">
+           <font face="Verdana">
+             LTC Stops on <xsl:value-of select="$street"/>
+           </font>
+          </xsl:if>
+          
+          <xsl:if test="$street=''">
+            <font face="Verdana">
+              All LTC Stops
+            </font>
+          </xsl:if>
+       </h1>
+        
+        <h3>
+          <xsl:value-of select="count(//stop[contains(@name , $street)])"/> stops found
+        </h3>
+        
+        <table style="width:720px" border="5">
+          <tr>
+            <th>
+              <font face="Verdana" size="4">Stop #</font>
+            </th>
+            <th>
+              <font face="Verdana" size="4">Stop Name</font>
+            </th>
+            <th>
+              <font face="Verdana" size="4">Latitude</font>
+            </th>
+            <th>
+              <font face="Verdana" size="4">Longitude</font>
+            </th>
+            <th>
+              <font face="Verdana" size="4">Routes</font>
+            </th>
+          </tr>
+          <xsl:apply-templates select ="allstops" />
+        </table>
+        </body>
+        </html>
+    </xsl:template>
+
+
+  <xsl:template match="allstops">
+    <!--loop the content-->
+   <xsl:for-each select="stop">
+    <xsl:sort select="@number" data-type="number"/>
+     <!--then, show the all or the special-->
+     <xsl:if test="contains(@name, $street)">
+      <xsl:element name="tr">
+        <xsl:element name="td">
+          <xsl:value-of select="@number"/>
+        </xsl:element>
+        <xsl:element name="td">
+          <xsl:value-of select="@name"/>
+        </xsl:element>
+        <xsl:element name="td">
+          <xsl:value-of select="location/latitude"/>
+        </xsl:element>
+        <xsl:element name="td">
+          <xsl:value-of select="location/longitude"/>
+        </xsl:element>
+        <xsl:element name="td">
+          <xsl:value-of select="routes"/>
+        </xsl:element>
+      </xsl:element>
+     </xsl:if>
+   </xsl:for-each>
+  </xsl:template>
+
+</xsl:stylesheet>
